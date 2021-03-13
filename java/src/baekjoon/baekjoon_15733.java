49ff260287f59e3e8f2173495e424eab2ee31e6c
Changhun-Han <hch4102@naver.com>
2021-03-14 02:08:12 +0900

    feat(Changhun-Han): [BOJ, 15733] 나는 누구인가
    https://www.acmicpc.net/problem/15733
    Signed-off-by: Changhoon-Han <hch4102@naver.com>

---
+++ java/src/baekjoon/baekjoon_15733.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/baekjoon_15733">https://www.acmicpc.net/problem/baekjoon_15733</a>
+ */
+public class baekjoon_15733 {
+    
+    public static void main (String[] args) {
+        System.out.println("I'm Sexy");
+    }
+    
+}

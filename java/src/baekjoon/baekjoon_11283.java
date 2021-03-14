87cb3b4e44bc21933cb3664363d40160d938318f
Changhun-Han <hch4102@naver.com>
2021-03-14 10:28:55 +0900

    feat(Changhun-Han): [BOJ, 11283] 한글 2
    https://www.acmicpc.net/problem/11283

---
+++ java/src/baekjoon/baekjoon_11283.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/11283">https://www.acmicpc.net/problem/11283</a>
+ */
+public class baekjoon_11283 {
+    
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(str.codePointAt(0) - 44031);
+        scanner.close();
+    }
+    
+}

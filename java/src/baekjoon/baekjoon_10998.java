4b0730f4596bc8be7e9144f43c439df3017648ec
Changhun-Han <hch4102@naver.com>
2021-03-14 09:56:13 +0900

    feat(Changhun-Han): [BOJ, 10998] A x B
    https://www.acmicpc.net/problem/10998

---
+++ java/src/baekjoon/baekjoon_10998.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/10998">https://www.acmicpc.net/problem/10998</a>
+ */
+public class baekjoon_10998 {
+    
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A * B);
+        scanner.close();
+    }
+    
+}
